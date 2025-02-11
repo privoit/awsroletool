161e4500e6fb0b9686c2977e4fb894b1878254d7
Matt McGowan <matthew.mcgowan@accenture.com>
2025-02-11 15:11:36 -0500

    Adding homebrew formula

---
+++ Formula/aws-role-tool.rb
@@ -0,0 +1,24 @@
+class AwsRoleTool < Formula
+  desc "AWS Role Assumption Tool"
+  homepage "https://github.com/privoit/awsroletool"
+  version "1.0.0"
+  url "https://github.com/privoit/awsroletool/releases/download/v1.0.0/AWS.Role.Tool-1.0.0-arm64.dmg"
+  sha256 "19461526f140a11e80cebd619ead41a5ca57a09ddb072d51799d9c6a7cacb02a"
+
+  def install
+    system "hdiutil", "attach", "AWS-Role-Tool-1.0.0-arm64.dmg"
+    system "cp", "-r", "/Volumes/AWS Role Tool/AWS Role Tool.app", "#{prefix}/AWS Role Tool.app"
+    system "hdiutil", "detach", "/Volumes/AWS Role Tool"
+  end
+
+  def caveats
+    <<~EOS
+      After installing, you may need to run:
+      brew link --force aws-role-tool
+    EOS
+  end
+
+  def uninstall
+    system "rm", "-rf", "#{prefix}/AWS Role Tool.app"
+  end
+end
