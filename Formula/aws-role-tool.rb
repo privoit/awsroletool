c619298af6f286f252eb57e316b6972215e87e99
Matt McGowan <matthew.mcgowan@accenture.com>
2025-02-11 16:02:37 -0500

    updating homebrew formula

--- Formula/aws-role-tool.rb
+++ Formula/aws-role-tool.rb
@@ -2,7 +2,7 @@ class AwsRoleTool < Formula
   desc "AWS Role Assumption Tool"
   homepage "https://github.com/privoit/awsroletool"
   version "1.0.0"
-  url "https://github.com/privoit/awsroletool/releases/download/v1.0.0/AWS.Role.Tool-1.0.0-arm64.dmg"
+  url "https://github.com/privoit/awsroletool/releases/download/v1.3.1/AWS.Role.Tool-1.0.0-arm64.dmg"
   sha256 "19461526f140a11e80cebd619ead41a5ca57a09ddb072d51799d9c6a7cacb02a"
 
   def install
